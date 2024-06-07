b0e10a8ca68da0bd1305716dcfbdca111fd8adcd
Jordan J <jordanr2m@gmail.com>
2024-06-07 10:07:43 -0700

    Wrote JSX to map over testimonials.

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -1,35 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
 import "./Testimonial.css";
 import { FaQuoteLeft } from "react-icons/fa";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { users } from './data';
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   return (
-    <div className='slider'>
-      <h1>Testimonial Section</h1>
-      <FaQuoteLeft />
-      
-      {users.map((user, index) => {
-        return (
-          <div className='slide current' key={index}>
-            <>
-              <div className='content'>
-                <p>{user.desc}</p>
+    <section className='main --flex-center'>
+      <div className='container --text-center'>
+        <h2 className='--text-center --mb2'>Testimonial Section</h2>
+
+        <div className='slider'>
+          <FaQuoteLeft className='icon' />
+
+          {users.map((user, index) => {
+            return (
+              <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
+                {/* If index = current slide, then show it */}
+                {index === currentSlide && (
+                  <div>
+                    <p>{user.desc}</p>
+                    <hr />
+                    <img src={user.img} alt="profile" />
+                    <h4>{user.name}</h4>
+                    <p className='--fw-thin'>{user.job}</p>
+                  </div>
+                )}
+
+                <div className='buttons'>
+                  <AiOutlineArrowLeft size={15} className='prev' />
+                  <AiOutlineArrowRight size={15} className='next' />
+                </div>
+
               </div>
-
-              <img src={user.img} alt="profile"/>
-              <hr />
-              <h3>{user.name}</h3>
-              <p>{user.job}</p>
-            </>
-          </div>
-        )
-      })}
-
-      <AiOutlineArrowLeft />
-      <AiOutlineArrowRight />
-    </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonial/data.js
+++ src/components/testimonial/data.js
@@ -1,14 +1,14 @@
 export const users = [
     {
-      img: "https://images.pexels.com/photos/1181497/pexels-photo-1181497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      img: "https://i.ibb.co/RzVhF9f/image-tanya.jpg",
       name: "Tanya Sinclair",
       job: "UX Engineer",
       desc: " “I've been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I'm now in the job of my dreams and so excited about the future. ”",
     },
-    {
-      img: "https://i.ibb.co/L6vBVVw/image-john.jpg",
-      name: "John Tarkpor",
-      job: "Front-end Developer",
-      desc: " “ If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
-    },
+    // {
+    //   img: "https://i.ibb.co/L6vBVVw/image-john.jpg",
+    //   name: "John Tarkpor",
+    //   job: "Front-end Developer",
+    //   desc: " “ If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    // },
   ];
